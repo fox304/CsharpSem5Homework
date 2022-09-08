99e4241777bc40d229753117972600addeac5fc5
Maks <nordmax69@gmail.com>
2022-09-08 18:46:06 +0700

    сделал копию массива - 3-я задачка

---
+++ homework3/Program.cs
@@ -0,0 +1,46 @@
+﻿// Написать программу копирования массива
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+void Fill(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+void Print(int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Copy(int [,] array,int [,] array2)
+{
+    for (int i = 0; i < array2.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2.GetLength(1); j++)
+        {
+            array2[i,j] = array[i,j];
+        }
+    }
+}
+
+int [,] massive = new int [10,10];
+int [,] massive2 = new int [10,10];
+
+Fill(massive); // заполняем двумерный массив
+Print(massive); // печатаем первый массив
+Console.WriteLine();
+Copy(massive,massive2);// делаем копию
+Print(massive2);//печатаем второй массив
