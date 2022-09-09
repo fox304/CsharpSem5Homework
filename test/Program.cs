8e6c0d62f8909cb25a5be9d814566fd9574995be
fox304 <110406192+fox304@users.noreply.github.com>
2022-09-09 20:43:38 +0700

    deleted test

--- test/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿
-// тестовый режим
-// вывод точки вместо запятой
-using System.Globalization;
-IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
-double number = double.Parse("23.56", formatter);
-//ввод с телефона
